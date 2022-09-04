7a624e879d0cd34ecf9c5c79118bf710748ed86a
Ali Mulondo <myalimul@gmail.com>
2022-09-04 19:39:45 +0100

    Initial commit

---
+++ src/main/java/com/zaictronics/GameOfThroneHouses/GameOfThroneHousesApplication.java
@@ -0,0 +1,13 @@
+package com.zaictronics.GameOfThroneHouses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfThroneHousesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfThroneHousesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaictronics/GameOfThroneHouses/GameOfThroneHousesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaictronics.GameOfThroneHouses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfThroneHousesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
