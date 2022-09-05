e5a2b3d9618cf7c8164e65b88febe44e6b607c52
Ali Mulondo <myalimul@gmail.com>
2022-09-05 06:15:15 +0100

    create HouseController failing test

--- src/main/java/com/zaictronics/GameOfThroneHouses/controller/HouseController.java
+++ src/main/java/com/zaictronics/GameOfThroneHouses/controller/HouseController.java
@@ -3,22 +3,19 @@ package com.zaictronics.GameOfThroneHouses.controller;
 import com.zaictronics.GameOfThroneHouses.model.HouseModel;
 import com.zaictronics.GameOfThroneHouses.service.HousesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping(path ="/api/v1")
 public class HouseController {
 
     @Autowired
     HousesService houseService;
 
-    @GetMapping("/houses/{searchkey}")
-    public List<HouseModel> getHouses(@PathVariable String searchkey){
+    @GetMapping("/houses")
+    public List<HouseModel> getHouses(@RequestParam( "searchKey") String searchkey){
 
         List<HouseModel> houses = houseService.getHouses(searchkey);
 
--- src/test/java/com/zaictronics/GameOfThroneHouses/controller/HousesControllerWebLayerTest.java
+++ src/test/java/com/zaictronics/GameOfThroneHouses/controller/HousesControllerWebLayerTest.java
@@ -1,20 +1,37 @@
 package com.zaictronics.GameOfThroneHouses.controller;
 
+import com.zaictronics.GameOfThroneHouses.model.HouseModel;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 
 @WebMvcTest(controllers = HouseController.class)
+@AutoConfigureWebMvc()
 public class HousesControllerWebLayerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
     @Test
     @DisplayName("Houses can be returned")
     void testGetHouses_WhenSearchKeyIsProvided_returnsMatchingHouses(){
         //Arrange
+        String ulr = "http://localhost:8080/api/v1/houses?searchKey=test";
+        RequestEntity<Void> requestEntity = RequestEntity.get(ulr).build();
+        HouseModel houseModel = new HouseModel();
 
         //Act
+        ResponseEntity<HouseModel> response = testRestTemplate.exchange(requestEntity, HouseModel.class);
 
         //Assert
+        Assertions.assertEquals(houseModel, response.getBody());
 
     }
 }
