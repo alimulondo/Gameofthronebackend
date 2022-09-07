755d0e83c4589927bd2181017e8cc2ebe762a813
Ali Mulondo <myalimul@gmail.com>
2022-09-07 11:13:02 +0100

    Add passing test dummy service data

--- src/main/java/com/zaictronics/GameOfThroneHouses/shared/HouseList.java
+++ src/main/java/com/zaictronics/GameOfThroneHouses/shared/HouseList.java
@@ -3,6 +3,7 @@ package com.zaictronics.GameOfThroneHouses.shared;
 import com.zaictronics.GameOfThroneHouses.model.HouseModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,42 +13,42 @@ import java.util.Map;
 /*
 * Wrapper Class to return a list of houses
 * */
-@Configuration
+@Component
 public class HouseList {
 
-    @Bean
     public Map<String, String> getHouses() {
-       Map<String , String> dataSegmentOne =   Map.of(
-               "url", "https://www.anapioficeandfire.com/api/houses/1",
-               "name", "House Algood",
-               "region", "The Westerlands",
-               "coatOfArms", "A golden wreath, on a blue field with a gold border" +
-                       "(Azure, a garland of laurel within a bordure or)",
-               "words", "",
-               "titles", "[\" \"]",
-               "seats", "[\" \"]",
-               "currentLord", "",
-               "heir", "",
-               "overlord", "https://www.anapioficeandfire.com/api/houses/229"
+        Map<String, String> dataSegmentOne = Map.of(
+                "url", "https://www.anapioficeandfire.com/api/houses/1",
+                "name", "House Algood",
+                "region", "The Westerlands",
+                "coatOfArms", "A golden wreath, on a blue field with a gold border" +
+                        "(Azure, a garland of laurel within a bordure or)",
+                "words", "",
+                "titles", "[\" \"]",
+                "seats", "[\" \"]",
+                "currentLord", "",
+                "heir", "",
+                "overlord", "https://www.anapioficeandfire.com/api/houses/229"
         );
-       Map<String, String> dataSegmentTwo = Map.of(
-               "diedOut", "",
-               "ancestralWeapons", "[\" \"]",
-               "cadetBranches", "[\" \"]",
-               "swornMembers", "[\" \"]"
+        Map<String, String> dataSegmentTwo = Map.of(
+                "diedOut", "Just test data",
+                "ancestralWeapons", "[\" \"]",
+                "cadetBranches", "[\" \"]",
+                "swornMembers", "[\" \"]"
 
 
+        );
 
-      );
-
-       Map<String, String> transferData = new HashMap<>();
+        Map<String, String> transferData = new HashMap<>();
         transferData.putAll(dataSegmentOne);
         transferData.putAll(dataSegmentTwo);
 
-       return transferData;
+        return transferData;
 
     }
 
     public void setHouses(List<HouseModel> houseModel) {
     }
+
+
 }
--- src/test/java/com/zaictronics/GameOfThroneHouses/shared/HouseListTest.java
+++ src/test/java/com/zaictronics/GameOfThroneHouses/shared/HouseListTest.java
@@ -1,14 +1,30 @@
 package com.zaictronics.GameOfThroneHouses.shared;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HouseListTest {
 
+    @Autowired
+    HouseList dummyData;
+
     @Test
-    void getHouses() {
+    void testGetHouses_WhenMethodCallIsMade_returnDummyData() {
+        String expectedUrl = "https://www.anapioficeandfire.com/api/houses/1";
+        String expectedDiedOut = "Just test data";
+
+
+        String actualUrl = dummyData.getHouses().get("url");
+        String actualDiedOut = dummyData.getHouses().get("diedOut");
+
+        Assertions.assertEquals(expectedUrl, actualUrl, "Url should be "+expectedUrl);
+        Assertions.assertEquals(expectedDiedOut, actualDiedOut, "Url should be "+actualDiedOut);
+
     }
 }
